4f9b1b1b720e5cc0ac73249ffc4fe87647061ffa
Jie Zheng <jayie21@live.com>
2019-05-05 23:28:23 -0500

    MPI race condition

--- mpi/mpi.c
+++ mpi/mpi.c
@@ -20,12 +20,8 @@ typedef struct
 	uint32_t physicalMem;
 } processMem_t;
 
-/* This function is called in GetProcessMemory() to parse the parse
- * line and return the uint32_t value of it.
- */
 int parseLine(char *line)
 {
-	// This assumes that a digit will be found and the line ends in " Kb".
 	int i = strlen(line);
 	const char *p = line;
 	while (*p < '0' || *p > '9') p++;
@@ -34,8 +30,7 @@ int parseLine(char *line)
 	return i;
 }
 
-/* Calculates the physical and virtual memory used.
- */
+/* Memory usage for both virtual and physical*/
 void GetProcessMemory(processMem_t* processMem)
 {
 	FILE *file = fopen("/proc/self/status", "r");
@@ -43,7 +38,6 @@ void GetProcessMemory(processMem_t* processMem)
 
 	while (fgets(line, 128, file) != NULL)
 	{
-		//printf("%s", line);
 		if (strncmp(line, "VmSize:", 7) == 0)
 		{
 			processMem->virtualMem = parseLine(line);
@@ -59,15 +53,6 @@ void GetProcessMemory(processMem_t* processMem)
 
 int main(int argc, char *argv[])
 {
-	/* Know how many processes there are. Find even section size and BCast
-	 * the section size to all processes. Processes will know their section
-	 * by multiplying the section size by their process number / ID. From
-	 * there each process reads through wiki_dump.txt and finds their section,
-	 * allocating the appropriate memory for their lines. They each run
-	 * algorithm. Then they each transmit their portion of longestCommonSubstring
-	 * to process 0, the main process. A Barrier might be needed to synchronize
-	 * the processes among each other. From there need to print the output.
-	 */
 	 double elapsedTime = 0.0;
 	 struct timeval t1, t2;
 	 processMem_t myMem;
@@ -92,12 +77,6 @@ int main(int argc, char *argv[])
 	 MPI_Comm_size(MPI_COMM_WORLD, &numTasks);
 	 MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	 // TODO: don't forget to get time with rank 0
-
-	 //printf("size = %d rank = %d\n", numTasks, rank);
-	 //fflush(stdout); // fflush should accompany almost every printf. Helps guarantee output
-
-	 // rank 0 is task 1, numTasks is 1 greater than last rank.
 	 chunkSize = linesToProcess/numTasks;
 	 startIndex = chunkSize * rank;
 	 endIndex = (chunkSize * (rank + 1)) + 1; // + 1 to allocate the line for the last comparison, not last line used to compare
@@ -114,8 +93,6 @@ int main(int argc, char *argv[])
 		 size_t line_length = getline(&buffer, &n, fd);
 		 if(i >= startIndex & i < endIndex)
 		 {
-			 //printf("rank=%d i=%d\n", rank, i);
-			 //fflush(stdout);
 		   wiki_dump[j] = (char*)malloc((line_length) * sizeof(char));
 		   memcpy(wiki_dump[j], buffer, sizeof(char) * line_length);
 		   wiki_dump[j][line_length-2] = 0;
@@ -125,18 +102,11 @@ int main(int argc, char *argv[])
 	 fclose(fd);
 
 	 longestCommonSubstring = (char **) malloc((mallocSize)* sizeof(char *));
-	 /*
-	 printf("mallocSize = %d, chunkSize = %d\n", mallocSize, chunkSize);
-	 for(i = 0; i < mallocSize; i++)
-	 {
-		 printf("i = %d, %s\n", i, wiki_dump[i]);
-	 }
-	 */
 	 gettimeofday(&t1, NULL);
 	 algorithm(wiki_dump, longestCommonSubstring, chunkSize);
 
 	 MPI_Barrier(MPI_COMM_WORLD);
-	 /*
+
 	 if(rank == 0)
 	 {
 		 printData(longestCommonSubstring, chunkSize, rank);
@@ -154,7 +124,7 @@ int main(int argc, char *argv[])
 		 tag = TAG_DONE_PRINTING;
 		 MPI_Send(&tag, 1, MPI_LONG, 0, TAG_DONE_PRINTING, MPI_COMM_WORLD);
 	 }
-	 */
+
 
 	 gettimeofday(&t2, NULL);
 	 GetProcessMemory(&myMem);
@@ -184,15 +154,11 @@ void algorithm(char **wiki_dump, char **longestCommonSubstring, int chunkSize)
 		char *s2 = wiki_dump[j + 1];
 		s1_len = strlen(wiki_dump[j]);
 		s2_len = strlen(wiki_dump[j + 1]);
-		//printf("ThreadNum: %d; s1_len:%d; s2_len:%d\n", omp_get_thread_num(), s1, s2);
-		//int table[s1_len + 1][s2_len + 1];
 
 		int **table = (int **)malloc((s1_len + 1) * sizeof(int *));
 		for(i = 0; i < s1_len+1; i++)
 			table[i] = (int *)malloc((s2_len + 1) * sizeof(int));
 
-
-		//initialize outside of table to zeros
 		for(i = 0; i <= s1_len; i++)
 			table[i][0] = 0;
 		for(i = 0; i <= s2_len; i++)
@@ -219,7 +185,7 @@ void algorithm(char **wiki_dump, char **longestCommonSubstring, int chunkSize)
 			}
 		}
 
-		//create longest substring based on index of max value
+		//Create longestCommonSubstring based on the max value from table
 		if(max > 0)
 		{
 			char substr[max];
